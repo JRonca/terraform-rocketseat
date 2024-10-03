a190d9fa5fb823e485a27af5075a1aa5bfb30c7b
JRonca <jrfr21112000@gmail.com>
2024-10-03 09:41:29 -0300

    variables

--- primeiro-projeto-iac/main.tf
+++ primeiro-projeto-iac/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "snores-bucket-iac-${terraform.workspace}"
+    bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
 
     tags = {
         Name = "First bucket"
