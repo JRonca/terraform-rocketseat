ea3e662d1e0a596cf97c8eedc13351ed39f1a985
JRonca <jrfr21112000@gmail.com>
2024-10-08 16:29:56 -0300

    modules part 1

--- primeiro-projeto-iac/main.tf
+++ primeiro-projeto-iac/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
 
-    tags = {
-        Name = "First bucket"
-        Iac = true
-        context = "${terraform.workspace}"
-    }
+  tags = {
+    Name    = "First bucket"
+    Iac     = true
+    context = "${terraform.workspace}"
+  }
 }
