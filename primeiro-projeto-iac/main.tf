050411ae2b3a7041f58075eca747067647f525a1
JRonca <jrfr21112000@gmail.com>
2024-10-17 16:48:22 -0300

    new variables

--- primeiro-projeto-iac/main.tf
+++ primeiro-projeto-iac/main.tf
@@ -1,12 +1,19 @@
 module "s3" {
   source         = "./modulos/s3"
   s3_bucket_name = "ronca-iac"
+  s3_tags = {
+    Iac = true
+  }
 }
 
 module "cloudfront" {
   source             = "./modulos/cloudfront"
   origin_id          = module.s3.bucket_id
   bucket_domain_name = module.s3.bucket_domain_name
+  cdn_price_class    = "PriceClass_200"
+  cdn_tags = {
+    Iac = true
+  }
   depends_on = [
     module.s3
   ]
--- primeiro-projeto-iac/modulos/cloudfront/variables.tf
+++ primeiro-projeto-iac/modulos/cloudfront/variables.tf
@@ -7,3 +7,16 @@ variable "bucket_domain_name" {
   type        = string
   description = "Domínio do S3"
 }
+
+variable "cdn_price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "Classe de preço do CDN"
+}
+
+variable "cdn_tags" {
+  type        = map(string)
+  default     = {}
+  description = "description"
+}
+
