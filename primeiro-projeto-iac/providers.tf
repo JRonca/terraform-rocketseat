9152a4d67ec49e5ad991554f51e4083757d61836
JRonca <jrfr21112000@gmail.com>
2024-10-27 23:30:44 -0300

    state at S3

--- primeiro-projeto-iac/providers.tf
+++ primeiro-projeto-iac/providers.tf
@@ -5,8 +5,36 @@ terraform {
       version = "5.66.0"
     }
   }
+  backend "s3" {
+    bucket  = "snores-state-bucket-tf"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = "roncaAdmAccess"
+  }
+
 }
 
 provider "aws" {
   profile = "roncaAdmAccess"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [
+    aws_s3_bucket.terraform_state
+  ]
 }
---
+++ primeiro-projeto-iac/variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "snores-state-bucket-tf"
+  description = "Bucket com o estado"
+}
